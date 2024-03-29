6102bad34bacd496d956158d203372e376c3a4ee
rogertse <39270736+rogertse@users.noreply.github.com>
2024-03-29 20:53:15 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,13 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				8888.Fund
+				8848.club
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-				8888.Fund is an AI investment fund that leverages artificial intelligence to predict market trends and optimize investment strategies, resulting in a higher success rate for investments.{" "}
+				8848.club is an AI investment club that leverages artificial intelligence to predict market trends and optimize investment strategies, resulting in a higher success rate for investments.{" "}
 				
 				</h2>
 			</div>
