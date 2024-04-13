d83bdd59cf5da11b199828d742bbdd0b8e806b1f
rogertse <39270736+rogertse@users.noreply.github.com>
2024-04-13 12:52:30 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-				8848.club is a world's top investors' club in the web3 and AI fields.{" "}
+				8848.club is the world's top investors' club. {" "}
 				</h2>
 			</div>
 		</div>
