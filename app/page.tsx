676378f44ba9987550fb8388d23190dab2db5449
rogertse <39270736+rogertse@users.noreply.github.com>
2024-04-13 12:53:54 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-	{ name: "Products", href: "" },
+	{ name: "Discord", href: "https://discord.gg/RYvFVAtr" },
 	{ name: "Contact", href: "/contact" },
 ];
 
